a2c1c0cd9b328872687d7fed3f6c858409bc2f14
Sergio Garguir <37402181+TechMDQLabV@users.noreply.github.com>
2020-09-15 15:29:04 -0300

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,15 +10,26 @@ export class InfoPaginaService {
 
   info: InforPagina = {};
   cargada = false;
+  equipo: any[];
 
   constructor( private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     /// leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp:InforPagina) => {
           this.cargada = true;
           this.info = resp;
       });
+  }
 
+  private cargarEquipo(){
+    this.http.get('https://angular-portafolio-c2f0d.firebaseio.com/equipo.json')
+      .subscribe((resp: any[])  => {
+          this.equipo = resp;
+      })
   }
 }
